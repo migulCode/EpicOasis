0dc7602be6fe6c12d3a44db0bf5f6f9db8818137
miguelcode <miguelpoter22@gmail.com>
2025-04-17 22:15:25 -0500

    refactor: update Tailwind CSS dependencies and remove config file
    - Removed @astrojs/tailwind dependency and added @tailwindcss/vite.
    - Updated tailwindcss version to 4.1.4.
    - Deleted tailwind.config.js as it is no longer needed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
