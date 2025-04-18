1111b6acfcda16890b3706161a12d530b71ff630
miguelcode <miguelpoter22@gmail.com>
2025-04-17 21:52:12 -0500

    chore: update dependencies for Astro and Tailwind CSS
    - Added @astrojs/tailwind dependency (v6.0.2)
    - Updated tailwindcss to version 4.1.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
