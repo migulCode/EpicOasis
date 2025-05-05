b89c1f6a082c240a56bd091d27da9ba12c8a465f
Miguel Lopez <miguelpoter22@gmail.com>
2025-05-04 23:17:25 -0500

    feat: mejorar estilos y lógica en Header, implementar cambio de tema, y optimizar animaciones en Slider

--- src/components/games/List.jsx
+++ src/components/games/List.jsx
@@ -14,10 +14,9 @@ function List({ allGames }) {
         <div
           key={play.id}
           style={{ animation: `var(--animate-loader) ${index * 0.1}s` }}
-          className="flex flex-col items-center p-3 w-[30%] min-w-[350px] max-w-[400px] bg-claro-tarjeta border-claro-borde dark:bg-oscuro-tarjeta dark:border-oscuro-borde rounded-lg shadow-md
-          max-md:w-[90%] max-md:min-w-none max-md:max-w-[600px] opacity-0"
+          className="flex flex-col items-center p-3 min-w-[300px] max-w-[400px] bg-claro-tarjeta border-claro-borde dark:bg-oscuro-tarjeta dark:border-oscuro-borde rounded-lg shadow-md max-md:w-[90%] max-md:min-w-none max-md:max-w-[600px] opacity-0"
         >
-          <h2 className="w-[90%] text-center text-xl">{play.title}</h2>
+          <h2 className="w-[90%] text-center">{play.title}</h2>
           <figure className="mt-auto mb-3 w-full aspect-94/53 min-h-50">
             <img
               className="size-full object-cover object-top"
